b848ee2f80eeff0ab31370f698f0d65731ae81f5
Ravi <ravi@ravis-mbp.localdomain>
2022-08-26 01:16:50 +0530

    Added nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <nav className="uk-navbar-container" uk-navbar>
+        <div className="uk-navbar-left">
+          <ul className="uk-navbar-nav">
+            <li className="uk-active"><a href="">Test</a></li>
+            <li className="uk-parent"><a href="">Test 2</a></li>
+            <li><a href="">Test 3</a></li>  
+          </ul>
+        </div>
+      </nav>
+      <div className="uk-child-width-1-2@s uk-text-center" uk-grid>
+        <div>
+          <div className="uk-background-default uk-padding uk-panel">
+            <p className="uk-h4">Default</p>
+          </div>
+        </div>
+        <div>
+          <div className="uk-background-muted uk-padding uk-panel">
+            <p className="uk-h4">Muted</p>
+          </div>
+        </div>
+        <div>
+          <div className="uk-background-primary uk-light uk-padding uk-panel">
+            <p className="uk-h4">Primary</p>
+          </div>
+        </div>
+        <div>
+          <div className="uk-background-secondary uk-light uk-padding uk-panel">
+            <p className="uk-h4">Secondary</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
