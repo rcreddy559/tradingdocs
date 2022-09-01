ad4b265984b93388289afc913c3325b9b220afe4
Ravichandra Reddy P <rc.reddy@outlook.com>
2022-09-02 01:18:54 +0530

    Added some more pages

--- src/components/content/candelsticks/patterns/hammer/HammerPattern.tsx
+++ src/components/content/candelsticks/patterns/hammer/HammerPattern.tsx
@@ -1,23 +1,39 @@
 
 export const HammerPattern = () => {
     return <article className="uk-article">
-    <h1 className="uk-article-title">Hammer Candlestick Pattern</h1>
-    <p className="uk-article-meta">The <b>hammer</b> and the <b>inverted hammer</b> candlestick patterns are among the most popular trading formations. 
+        <h1 className="uk-article-title">Hammer Candlestick Pattern</h1>
+        <p className="uk-article-meta">
+            The <b>hammer</b> and the <b>inverted hammer</b> candlestick patterns are among the most popular trading formations.
+            Both are reversal patterns, and they occur at the <b>bottom of a downtrend.</b>
+        </p>
+        <p className="uk-text-lead">
+            The <b>Hammer pattern </b>is a single-candle bullish reversal pattern that can be spotted at the end of
+            a downtrend. The opening price, close, and top are approximately at the same price, while there is a long wick that extends lower,
+            twice as big as the short body.
+        </p>
+        <img src="./candlesticks/patterns/shooting-star/ShootingStarPic.gif" />
+        <p>
+            Hammer candles are formed when the open, high, and close values are similar. However, a long wick or shadow indicates that
+            the price has reached significantly lower values before closing the candle. Hammer candles can appear as either red or green candles,
+            with the most important factor being the ratio of the shadow to the body of the candle.
+            The standard accepted among technical traders is that the wick under the candle body is at least 2 times as long.</p>
+        <h2>What does Hammer Candlestick Pattern tell you?</h2>
+        <ul className="imp-points">
+            <li>Hammer is formed after the stock prices have been falling, indicating that the prices are attempting to form a bottom.</li>
+            <li>Hammers signal that the bears have lost control over the prices, indicating a potential reversal to an uptrend.</li>
+            <li>One should note that this candlestick should be formed after three or more bearish candles as it gives more confirmation.</li>
+        </ul>
+        <h3>Trading Example:</h3>
+        <ol>
+            <li>Before forming a Hammer pattern, the prior trend should be a uptrend, and there should be at least 2-3 bullish candlesticks.</li>
+            <li>The Hammer pattern is formed when the real body is small with a long lower shadow.</li>
+            <li>To confirm the reversal, a bearish candlestick should be formed after the Hammer.</li>
+        </ol>
 
- 
-Both are reversal patterns, and they occur at the <b>bottom of a downtrend.</b> </p>
-
-    <p className="uk-text-lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-    <div className="uk-grid-small uk-child-width-auto" uk-grid>
-        <div>
-            <a className="uk-button uk-button-text" href="#">Read more</a>
-        </div>
-        <div>
-            <a className="uk-button uk-button-text" href="#">5 Comments</a>
-        </div>
-    </div>
-</article>
+        <img src="./candlesticks/patterns/hammer/bullish-hammer-chart.png" />
+        <img src="./candlesticks/patterns/hammer/bullish-hammer-chart.png" />
+        <img src="./candlesticks/patterns/hammer/bullish-hammer-chart.png" />
+        <img src="./candlesticks/patterns/hammer/bullish-hammer-chart.png" />
+        <img src="./candlesticks/patterns/hammer/bullish-hammer-chart.png" />
+    </article>
 }
